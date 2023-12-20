92f3626a88c6ee0e8bc3097f3b1269c72485e01c
ianclarke03 <ianisian2003@gmail.com>
2023-12-19 23:23:14 -0500

    saving before further modification

--- Game_IC/main.cpp
+++ Game_IC/main.cpp
@@ -27,11 +27,16 @@ public:
 		else if (e.GetKeyCode() == EGG_KEY_LEFT)
 			//x -= 50;
 			mUnit.UpdateXCoord(-50);
+		//new addition by me
+		else if (e.GetKeyCode() == EGG_KEY_UP)
+			mUnit.UpdateYCoord(50);
+		else if (e.GetKeyCode() == EGG_KEY_DOWN)
+			mUnit.UpdateYCoord(-50);
 	}
 
 private:
-	egg::Picture mPic{ "../Assets/Pictures/test.png" };
-	egg::Unit mUnit{ "../Assets/Pictures/test.png" , 100, 500 };
+	egg::Picture mPic{ "../Assets/Pictures/bxcbiiu1wxa71.png" };
+	egg::Unit mUnit{ "../Assets/Pictures/bxcbiiu1wxa71.png" , 200, 400 };
 
 	int x{ 100 };
 	int y{ 100 };
